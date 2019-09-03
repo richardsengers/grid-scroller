5c4f434686c06a3ae16f49f38e334f6d3ca147b3
Richard <richard@araneum.nl>
2019-09-03 15:32:52 +0200

    fixes #1

--- src/app/core/api-http.service.spec.ts
+++ src/app/core/api-http.service.spec.ts
@@ -1,9 +1,18 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
 import { ApiHttpService } from './api-http.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ApiHttpService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [ApiHttpService]
+  }));
+
+  it('should be initialized', inject([ApiHttpService], (apiHttpService: ApiHttpService) => {
+    expect(ApiHttpService).toBeTruthy();
+  }));
 
   it('should be created', () => {
     const service: ApiHttpService = TestBed.get(ApiHttpService);
--- src/app/shared/tile/tile.component.spec.ts
+++ src/app/shared/tile/tile.component.spec.ts
@@ -1,13 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TileComponent } from './tile.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
+import { Image } from '../models/image.model';
+import { ElementRef } from '@angular/core';
+import { TooltipModule } from 'ng2-tooltip-directive';
 
 describe('TileComponent', () => {
   let component: TileComponent;
   let fixture: ComponentFixture<TileComponent>;
+  let title: ElementRef;
+  let titleEl: HTMLElement;
+  let expectedImage: Image;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [NoopAnimationsModule, TooltipModule],
       declarations: [ TileComponent ]
     })
     .compileComponents();
@@ -16,10 +25,30 @@ describe('TileComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TileComponent);
     component = fixture.componentInstance;
+
+    title = fixture.debugElement.query(By.css('.heading'));
+    titleEl = title.nativeElement;
+
+    expectedImage = new Image({
+      url: 'https://static.marcomprocloud.eu/account/4dac4777e37f501a80391ba3/cache/imagecloud/164ba537292d7bf874869f1ce7f321f5/03cb4ae46da302ebaf4fda886601dff339eaa381.jpg',
+      title: 'Ferrari',
+      name: 'ferrari.jpg',
+      description: 'Red sports car'
+    });
+
+    component.item = expectedImage;
+
     fixture.detectChanges();
   });
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display Ferrari as title', () => {
+    const expectedTitle = expectedImage.title;
+    expect(titleEl.textContent).toContain(expectedTitle);
+  });
+
 });
